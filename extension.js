727ef59086b7722365e5eead746b517b0115c0c5
Anton Urakov <AntonUrakov@gmail.com>
2023-04-26 16:43:30 +0300

    v0.01

--- extension.js
+++ extension.js
@@ -5,27 +5,27 @@ const vscode = require('vscode');
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
+// /**
+//  * @param {vscode.ExtensionContext} context
+//  */
+// function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ewrapper" is now active!');
+// 	// Use the console to output diagnostic information (console.log) and errors (console.error)
+// 	// This line of code will only be executed once when your extension is activated
+// 	console.log('Congratulations, your extension "ewrapper" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ewrapper.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+// 	// The command has been defined in the package.json file
+// 	// Now provide the implementation of the command with  registerCommand
+// 	// The commandId parameter must match the command field in package.json
+// 	let disposable = vscode.commands.registerCommand('ewrapper.helloWorld', function () {
+// 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from _e wrapper!');
-	});
+// 		// Display a message box to the user
+// 		vscode.window.showInformationMessage('Hello World from _e wrapper!');
+// 	});
 
-	context.subscriptions.push(disposable);
-}
+// 	context.subscriptions.push(disposable);
+// }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
@@ -33,4 +33,27 @@ function deactivate() {}
 module.exports = {
 	activate,
 	deactivate
+}
+
+
+import * as vscode from 'vscode';
+
+export function activate(context) {
+  let disposable = vscode.commands.registerCommand('extension.wrapWithTranslation', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+
+    const selection = editor.selection;
+    const text = editor.document.getText(selection);
+
+    const wrappedText = `{{ _e('${text}') }}`;
+
+    editor.edit(editBuilder => {
+      editBuilder.replace(selection, wrappedText);
+    });
+  });
+
+  context.subscriptions.push(disposable);
 }
